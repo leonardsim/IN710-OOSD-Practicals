bc1d40f80b2aae8fcb9ba6d1176c306d0cc806ce
leonardsim <simle1@STUDENT.op.ac.nz>
2016-02-16 21:28:10 +1300

    TwoToOne
    Took content out of a 2 dimensional array and added it into a 1
    dimensional array in sequence

---
+++ Session 1/TwoToOne/TwoToOne/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwoToOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         public void TwoToOne()
+ {
+    const int columnCount = 4;
+    const int rowCount = 2;
+
+	Char[] oneDimensional = new Char[columnCount * rowCount];
+    Char[,] twoDimensional = new Char[columnCount, rowCount] 
+          { { 'a', 'b' }, { 'c', 'd' }, { 'e', 'f' }, { 'g', 'h' } };
+
+    // convert to one dimension
+    for (int c = 0; c < columnCount; c++)
+        for (int r = 0; r < rowCount; r++)
+        {
+            //Add code here. DO NOT declare any new variables
+            if (r == 0)
+            {
+                oneDimensional[c * rowCount] = twoDimensional[c, r];
+            }
+            else
+            {
+                oneDimensional[c * rowCount + r] = twoDimensional[c, r];
+            }
+        }
+
+    //printing one dimensional array
+    for (int i = 0; i < columnCount * rowCount; i++)
+        listBox1.Items.Add(oneDimensional[i]);
+}
+
+         private void button1_Click(object sender, EventArgs e)
+         {
+             TwoToOne();
+         }
+
+    }
+}
