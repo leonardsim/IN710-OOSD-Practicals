271d8abe4c62022fd7811965d7a383e16152188e
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-07 14:22:55 +1300

    Created ArrayStack Project
    Contains Stack class with a Push function and a PrintStack class

---
+++ StackUnitTesting/StackUnitTesting/ArrayStack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUnitTesting
+{
+    public class ArrayStack
+    {
+        //Constant
+        const int ARRAY_SIZE = 5;
+
+        //Variables
+        String[] stackString;
+        int countArray;
+
+        //Constructor
+        public ArrayStack()
+        {
+            stackString = new String[ARRAY_SIZE];
+            countArray = -1;
+        }
+
+        //Methods
+        public void Push(string newString)
+        {
+            countArray++;
+            stackString[countArray] = newString;
+        }
+
+        public String Pop()
+        {
+
+        }
+
+        //Get/Set methods
+        public String[] StackString
+        {
+            get { return stackString; }
+            set { stackString = value; }
+        }
+
+        public int CountArray
+        {
+            get { return countArray; }
+            set { countArray = value; }
+        }
+    }
+}
