2943d2add8d8f01f9f15e9b36abcf826183a2c05
leonardsim <SIMLE1@student.op.ac.nz>
2016-03-08 15:04:03 +1300

    Added classes and interface

---
+++ Game Character Skeleton/Game Character/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    public abstract class Character
+    {
+        //Variables
+        protected string name;
+
+        //Constructor
+        public Character(string name)
+        {
+            this.name = name;
+        }
+
+        //Methods
+        public virtual string StateName()
+        {
+            return "My name is " + name + "!";
+        }
+
+        public abstract string Declaim();
+    }
+}
---
+++ Game Character Skeleton/Game Character/King.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Character
+{
+    class King : Character, IWeapon
+    {
+
+        //Constructor
+        public King(string name) : base(name)
+        {
+            this.name = name;
+        }
+
+        //Methods
+        public override string Declaim()
+        {
+            return "I am the most mighty of Kings!";
+        }
+    }
+}
