3bc9e580af116202304a2592c2c2cea2943879b4
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-08 20:30:53 +1300

    Made addCharacter function for buttonAdd

--- Game Character Skeleton/Game Character/Form1.cs
+++ Game Character Skeleton/Game Character/Form1.cs
@@ -12,9 +12,49 @@ namespace Game_Character
 {
     public partial class Form1 : Form
     {
+        //Declare list of characters
+        List<Character> charactersList = new List<Character>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAddCharacter_Click(object sender, EventArgs e)
+        {
+            addCharacters();
+        }
+
+        public void addCharacters()
+        {
+            Character classType;
+
+            if (rdKing.Checked)
+            {
+                classType = new King(txtName.Text);
+                charactersList.Add(classType);
+            }
+            if (rdQueen.Checked)
+            {
+                classType = new Queen(txtName.Text);
+                charactersList.Add(classType);
+            }
+            if (rdKnight.Checked)
+            {
+                classType = new Knight(txtName.Text);
+                charactersList.Add(classType);
+            }
+            if (rdTroll.Checked)
+            {
+                classType = new Troll(txtName.Text);
+                charactersList.Add(classType);
+            }
+
+            foreach (Character chr in charactersList)
+            {
+                checkedListBox1.Items.Add(chr.Name);
+                checkedListBox2.Items.Add(chr.Name);
+            }
         }
     }
 }
