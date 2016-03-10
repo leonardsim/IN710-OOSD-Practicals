51ac38a81b205d258dbf83a56956c0adaf83eacf
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 19:02:22 +1300

    Made AnalogClock class that implements the interface methods

---
+++ AnalogDigitalClock/AnalogDigitalClock/AnalogClock.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalogDigitalClock
+{
+    public class AnalogClock : IClock
+    {
+        //Variables
+        AnalogClockControl.AnalogClock analogClock;
+
+        //Constructor
+        public AnalogClock(AnalogClockControl.AnalogClock analogClock)
+        {
+            this.analogClock = analogClock;
+        }
+
+        //Methods
+        // Starts the analog clock
+        public void On()
+        {
+            analogClock.Start();
+        }
+
+        // Stops the analog clock
+        public void Off()
+        {
+            analogClock.Stop();
+        }
+
+        // This method is not used
+        public void UpdateTimeDisplay()
+        {
+
+        }
+
+        // Turns visibility of analog clock on
+        public void ShowClock()
+        {
+            analogClock.Visible = true;
+        }
+
+        // Turns visibility of analog clock off
+        public void HideClock()
+        {
+            analogClock.Visible = false;
+        }
+    }
+}
