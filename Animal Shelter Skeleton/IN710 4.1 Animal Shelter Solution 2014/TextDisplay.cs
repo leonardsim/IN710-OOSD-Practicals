63b23589709e907e492db125a6970e567d725789
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 23:21:41 +1300

    Added interface methods to the TextDisplay class

--- Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
+++ Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
@@ -8,6 +8,31 @@ namespace IN710_4._1_Animal_Shelter_Solution_2014
 {
     class TextDisplay: IDisplay
     {
-        /* YOUR CODE HERE */
+        //Variables
+        ListBox animalList;
+
+        //Constructor
+        public TextDisplay(ListBox animalList)
+        {
+            this.animalList = animalList;
+        }
+
+
+        //Methods
+        // Displays the name and species of animal onto the listBox
+        public void displayCritterList(List<Critter> critterList)
+        {
+            // foreach critter in the list ,add the name and sepcies to the listBox
+            foreach (Critter c in critterList)
+            {
+                animalList.Items.Add(c.Name + ": " + c.Species);
+            }
+        }
+
+        // Clear the listbox
+        public void clearDisplay()
+        {
+            animalList.Items.Clear();
+        }
     }
 }
