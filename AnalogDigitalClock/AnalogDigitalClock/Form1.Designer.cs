4e302f36f1d034c0650e46b214e9755e8739db0c
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 18:32:34 +1300

    Made the Design for AnalogDigitalClock

---
+++ AnalogDigitalClock/AnalogDigitalClock/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AnalogDigitalClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lblDigital = new System.Windows.Forms.Label();
+            this.analogClock1 = new AnalogClockControl.AnalogClock();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(389, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(90, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Digital";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(389, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(122, 30);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Analogue";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(175, 44);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start Clock";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(193, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(175, 44);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop Clock";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // lblDigital
+            // 
+            this.lblDigital.AutoSize = true;
+            this.lblDigital.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDigital.Location = new System.Drawing.Point(73, 164);
+            this.lblDigital.Name = "lblDigital";
+            this.lblDigital.Size = new System.Drawing.Size(347, 91);
+            this.lblDigital.TabIndex = 4;
+            this.lblDigital.Text = "00:00:00";
+            // 
+            // analogClock1
+            // 
+            this.analogClock1.Draw1MinuteTicks = true;
+            this.analogClock1.Draw5MinuteTicks = true;
+            this.analogClock1.HourHandColor = System.Drawing.Color.DarkMagenta;
+            this.analogClock1.Location = new System.Drawing.Point(98, 83);
+            this.analogClock1.MinuteHandColor = System.Drawing.Color.Green;
+            this.analogClock1.Name = "analogClock1";
+            this.analogClock1.SecondHandColor = System.Drawing.Color.Red;
+            this.analogClock1.Size = new System.Drawing.Size(322, 322);
+            this.analogClock1.TabIndex = 5;
+            this.analogClock1.TicksColor = System.Drawing.Color.Black;
+            this.analogClock1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 441);
+            this.Controls.Add(this.analogClock1);
+            this.Controls.Add(this.lblDigital);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lblDigital;
+        private AnalogClockControl.AnalogClock analogClock1;
+    }
+}
+
