132e1d7abc560f477d288786a3d04d56ed30130d
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 15:42:22 +1300

    Setup the encryption method in the buttons

--- EncryptionTool/EncryptionTool/Form1.cs
+++ EncryptionTool/EncryptionTool/Form1.cs
@@ -12,9 +12,51 @@ namespace EncryptionTool
 {
     public partial class Form1 : Form
     {
+        //Call interface as type variable
+        IEncryption encryptionMachine;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        // Encrypts the user input
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            // The classes that implement the interface IEncryption will be saved into the 
+            // encryptionMachine depending on which radio button was checked
+            if (rdoROT13.Checked)
+            {
+                encryptionMachine = new ROT13Encryptor();
+            }
+            else
+            {
+                encryptionMachine = new ReverseEncryptor();
+            }
+
+            // Whatever the user input in into the textbox will be saved, encrypted and then output inot the 2nd textbox
+            string userInput = txtInput.Text;
+            string encryptedMsg = encryptionMachine.Encrypt(userInput);
+            txtOutput.Text = encryptedMsg;
+        }
+
+        // Decrypt functions the same as the encrypt for this case
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (rdoROT13.Checked)
+            {
+                encryptionMachine = new ROT13Encryptor();
+            }
+            else
+            {
+                encryptionMachine = new ReverseEncryptor();
+            }
+
+            string userInput = txtInput.Text;
+            string encryptedMsg = encryptionMachine.Encrypt(userInput);
+            txtOutput.Text = encryptedMsg;
+        }
+
+
     }
 }
