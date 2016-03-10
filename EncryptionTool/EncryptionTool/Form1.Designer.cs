bf2d3e05cf470569af6fbe008adb56c639f2e5aa
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 15:10:13 +1300

    Codded the Encrypt method for ROT13

--- EncryptionTool/EncryptionTool/Form1.Designer.cs
+++ EncryptionTool/EncryptionTool/Form1.Designer.cs
@@ -28,12 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.rdoROT13 = new System.Windows.Forms.RadioButton();
+            this.rdoReversal = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Encryption Algorithm";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(371, 19);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(112, 36);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(371, 61);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnDecrypt.Size = new System.Drawing.Size(112, 36);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // rdoROT13
+            // 
+            this.rdoROT13.AutoSize = true;
+            this.rdoROT13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoROT13.Location = new System.Drawing.Point(28, 37);
+            this.rdoROT13.Name = "rdoROT13";
+            this.rdoROT13.Size = new System.Drawing.Size(78, 24);
+            this.rdoROT13.TabIndex = 3;
+            this.rdoROT13.TabStop = true;
+            this.rdoROT13.Text = "RTO13";
+            this.rdoROT13.UseVisualStyleBackColor = true;
+            // 
+            // rdoReversal
+            // 
+            this.rdoReversal.AutoSize = true;
+            this.rdoReversal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoReversal.Location = new System.Drawing.Point(28, 61);
+            this.rdoReversal.Name = "rdoReversal";
+            this.rdoReversal.Size = new System.Drawing.Size(132, 24);
+            this.rdoReversal.TabIndex = 4;
+            this.rdoReversal.TabStop = true;
+            this.rdoReversal.Text = "String Reverse";
+            this.rdoReversal.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Input:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(403, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(80, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(403, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 242);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rdoReversal);
+            this.Controls.Add(this.rdoROT13);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.RadioButton rdoROT13;
+        private System.Windows.Forms.RadioButton rdoReversal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- EncryptionTool/EncryptionTool/ROT13Encryptor.cs
+++ EncryptionTool/EncryptionTool/ROT13Encryptor.cs
@@ -8,6 +8,50 @@ namespace EncryptionTool
 {
     public class ROT13Encryptor : IEncryption
     {
+        public string Encrypt(string input)
+        {
+            // Save string characters into char array
+            char[] arrayChar = input.ToCharArray();
+
+            for (int i = 0; i < arrayChar.Length; i++)
+            {
+                // Saves the content of arrayChar[i] as an int
+                int numVal = (int)arrayChar[i];  
+                
+                // Checks if it is within the character range 1- 26
+                if (numVal >= 'a' && numVal <= 'z')
+                {
+                    // If the character is further down than 'm' then deduct it by 13
+                    // Else add 13 to it
+                    if (numVal >'m')
+                    {
+                        numVal -= 13;
+                    }
+                    else
+                    {
+                        numVal += 13;
+                    }
+                }
+                else if (numVal >= 'A' && numVal <= 'Z') // Same concept as above except with Captital letters
+                {
+                    if (numVal > 'M')
+                    {
+                        numVal -= 13;
+                    }
+                    else
+                    {
+                        numVal += 13;
+                    }
+                }
+                // Convert the numVal back into a char and store it back into the array
+                arrayChar[i] = (char)numVal;
+            }
+
+            //return the encrypted string
+            string encString = new string(arrayChar);
+
+            return encString;
+        }
 
     }
 }
