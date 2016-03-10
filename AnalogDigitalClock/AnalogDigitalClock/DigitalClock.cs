095a4a1644e8282234c4e6d32da857fec977170e
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 18:33:09 +1300

    Made DigitalClock class that implements the interface

---
+++ AnalogDigitalClock/AnalogDigitalClock/DigitalClock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnalogDigitalClock
+{
+    public class DigitalClock : IClock
+    {
+        Label timeDisplayLabel;
+        Timer timer;
+
+        public DigitalClock(Label timeDisplayLabel, Timer timer)
+        {
+            this.timeDisplayLabel = timeDisplayLabel;
+            this.timer = timer;
+        }
+
+
+    }
+}
