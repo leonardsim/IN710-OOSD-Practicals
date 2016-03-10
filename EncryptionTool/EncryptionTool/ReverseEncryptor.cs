63361b298a355e0a44707575c08ba779f573ffc0
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 15:27:20 +1300

    Coded encrypt method for Reverse string

--- EncryptionTool/EncryptionTool/ReverseEncryptor.cs
+++ EncryptionTool/EncryptionTool/ReverseEncryptor.cs
@@ -8,6 +8,18 @@ namespace EncryptionTool
 {
     public class ReverseEncryptor : IEncryption
     {
+        public string Encrypt(string input)
+        {
+            // Save string characters into char array
+            char[] arrayChar = input.ToCharArray();
 
+            // Call the Array Reverse feature, which reverses the characters
+            Array.Reverse(arrayChar);
+
+            // Save and return the reversed string
+            string encString = new string(arrayChar);
+
+            return encString;
+        }
     }
 }
