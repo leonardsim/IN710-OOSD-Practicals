a6d830ec3cf90581823ffca2fcef1a327e75ffca
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-10 23:57:39 +1300

    Added interface methods to the PictureDisplay class

--- Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/PictureDisplay.cs
+++ Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/PictureDisplay.cs
@@ -9,6 +9,33 @@ namespace IN710_4._1_Animal_Shelter_Solution_2014
 {
     class PictureDisplay: IDisplay
     {
-       /* YOUR CODE HERE */
+        //Variables 
+        List<PictureBox> animalPictures;
+
+        //Constructor
+        public PictureDisplay(List<PictureBox> animalPictures)
+        {
+            this.animalPictures = animalPictures;
+        }
+
+        //Methods
+        // Displays the pictures of the animal onto the 4 picture boxes
+        public void displayCritterList(List<Critter> critterList)
+        {
+            for (int i = 0; i < critterList.Count; i++)
+            {
+                animalPictures[i].Image = Image.FromFile(critterList[i].ImageFileName);
+            }
+        }
+
+        // Clears the picture boxes
+        public void clearDisplay()
+        {
+            foreach (PictureBox p in animalPictures)
+            {
+                p.Image = null;
+            }
+
+        }
     }
 }
