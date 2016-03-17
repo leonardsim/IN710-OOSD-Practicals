40c07e2c898a9b6ae488df7152f3af7870e002c9
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-17 19:33:29 +1300

    Added using AnimalClasses folder

--- ZoologicalSim/ZoologicalSim/NorthAmericanAnimalFactory.cs
+++ ZoologicalSim/ZoologicalSim/NorthAmericanAnimalFactory.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZoologicalSim.AnimalClasses;
 
-namespace ZoologicalSim.AnimalClasses
+namespace ZoologicalSim
 {
     public class NorthAmericanAnimalFactory : IAnimalFactory
     {
