1627f0335ab5e9b1fb87041ea6dd612096950c92
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-17 19:35:18 +1300

    Added Australia and Asia AnimalFactory classes

---
+++ ZoologicalSim/ZoologicalSim/AsiaAnimalFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZoologicalSim.AnimalClasses;
+
+namespace ZoologicalSim
+{
+    public class AsiaAnimalFactory : IAnimalFactory
+    {
+        // I.method
+        public Animal createAnimal(int animalCode)
+        {
+            Animal newAnimal = null;
+
+            switch (animalCode)
+            {
+                case 0:
+                    newAnimal = new OrangUtan();
+                    break;
+                case 1:
+                    newAnimal = new Tiger();
+                    break;
+                case 2:
+                    newAnimal = new Panda();
+                    break;
+                case 3:
+                    newAnimal = new Elephant();
+                    break;
+            }
+
+            return newAnimal;
+        }
+    }
+}
---
+++ ZoologicalSim/ZoologicalSim/AustraliaAnimalFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZoologicalSim.AnimalClasses;
+
+namespace ZoologicalSim
+{
+    public class AustraliaAnimalFactory : IAnimalFactory
+    {
+        // I.method
+        public Animal createAnimal(int animalCode)
+        {
+            Animal newAnimal = null;
+
+            switch (animalCode)
+            {
+                case 0:
+                    newAnimal = new Kangaroo();
+                    break;
+                case 1:
+                    newAnimal = new Dingo();
+                    break;
+                case 2:
+                    newAnimal = new Crocodile();
+                    break;
+                case 3:
+                    newAnimal = new Koala();
+                    break;
+            }
+
+            return newAnimal;
+        }
+    }
+}
