56cd3b08193260823002a07fe2b7785223f401c6
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-17 18:14:08 +1300

    Added btnNA click to run simulation

--- ZoologicalSim/ZoologicalSim/Form1.cs
+++ ZoologicalSim/ZoologicalSim/Form1.cs
@@ -12,9 +12,31 @@ namespace ZoologicalSim
 {
     public partial class Form1 : Form
     {
+        //Constant
+        const int ANIMAL_COUNT = 3;
+
+        //Variables
+        Continent mCon;
+        Random rGen;
+        Graphics canvas;
+
         public Form1()
         {
             InitializeComponent();
+
+            rGen = new Random();
+            canvas = this.CreateGraphics();
+            mCon = new NorthAmerica(lbAnimals, rGen, ANIMAL_COUNT, canvas);
+        }
+
+        private void btnNA_Click(object sender, EventArgs e)
+        {
+            // Clear the graphics and listbox
+            lbAnimals.Items.Clear();
+            canvas.Clear(Form1.ActiveForm.BackColor);
+
+            // Run simulation
+            mCon.runSimulation();
         }
     }
 }
