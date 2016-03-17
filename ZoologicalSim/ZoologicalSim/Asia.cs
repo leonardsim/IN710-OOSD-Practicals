757d8d94750a3b79177d41f31f95518abff9a6e5
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-17 19:35:39 +1300

    Added Asia and Australia continent

---
+++ ZoologicalSim/ZoologicalSim/Asia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZoologicalSim
+{
+    public class Asia : Continent
+    {
+        //Constructor
+        public Asia(ListBox displayBox, Random rGen, int nAnimalTypes, Graphics canvas)
+            : base(displayBox, rGen, nAnimalTypes, canvas)
+        {
+            animalFactory = new AsiaAnimalFactory();
+        }
+    }
+}
