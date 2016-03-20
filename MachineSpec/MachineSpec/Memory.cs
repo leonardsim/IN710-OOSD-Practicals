64e958b624e0acfe63314982a67578fc075fab66
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-21 12:12:09 +1300

    Made Memory base class

---
+++ MachineSpec/MachineSpec/Memory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineSpec
+{
+    public class Memory
+    {
+        //Variables
+        protected string name;
+        protected string price;
+
+        //Method
+        public override string ToString()
+        {
+            return "$" + price + ", " + name;
+        }
+
+        //Getters/Setters
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+    }
+}
