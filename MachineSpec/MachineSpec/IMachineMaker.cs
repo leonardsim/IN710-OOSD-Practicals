16d3487226e3bfdf20e96532cf6c3c2a3c123965
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-21 12:19:04 +1300

    Made Interface

---
+++ MachineSpec/MachineSpec/IMachineMaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineSpec
+{
+    public interface IMachineMaker
+    {
+        CPU makeCPU();
+        GPU makeGPU();
+        Memory makeMemory();
+    }
+}
