d29edb20637b8ad9c3eca27ae332683129276908
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-21 17:48:29 +1300

    Made Monster Factory

---
+++ MixMatch/MixMatch/MonsterFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixMatch
+{
+    public class MonsterFactory : IMonsterParts
+    {
+        List<string> monstersStr = new List<string>()
+        {
+            "Fairy",
+            "Frankenstein",
+            "Skeleton",
+            "Vampire",
+            "Werewolf",
+            "Witch"
+        };
+
+        public Monster makeMonster(string type)
+        {
+            Monster currMon = null;
+
+            switch (type)
+            {
+                case "Fairy":
+                    currMon = new Fairy();
+                    break;
+                case "Frankenstein":
+                    currMon = new Frankenstein();
+                    break;
+                case "Skeleton":
+                    currMon = new Skeleton();
+                    break;
+                case "Vampire":
+                    currMon = new Vampire();
+                    break;
+                case "Werewolf":
+                    currMon = new Werewolf();
+                    break;
+                case "Witch":
+                    currMon = new Witch();
+                    break;
+            }
+
+            return currMon;
+        }
+
+        //Getters/Setters
+        public List<string> MonstersStr
+        {
+            get { return monstersStr; }
+            set { monstersStr = value; }
+        }
+    }
+}
