3061bf9bb2da48ba17739fa92d3b380d9092f1a0
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-21 13:28:57 +1300

    Made MachineSpecPrinter class

---
+++ MachineSpec/MachineSpec/MachineSpecPrinter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachineSpec
+{
+    class MachineSpecPrinter
+    {
+        //Variables
+        private IMachineMaker machineMaker;
+        private ListBox displayBox;
+
+        //Constructor
+        public MachineSpecPrinter(IMachineMaker machineMaker, ListBox displayBox)
+        {
+            this.machineMaker = machineMaker;
+            this.displayBox = displayBox;
+        }
+
+        //Method
+        public void printSpec()
+        {
+            CPU currCPU = machineMaker.makeCPU();
+            Memory currMemory = machineMaker.makeMemory();
+            GPU currGPU = machineMaker.makeGPU();
+
+            double totalPrice = currCPU.Price + currGPU.Price + currMemory.Price;
+
+            displayBox.Items.Clear();
+            displayBox.Items.Add("Price\tComponent");
+            displayBox.Items.Add("*********************************");
+
+            displayBox.Items.Add(currCPU.ToString());
+            displayBox.Items.Add(currGPU.ToString());
+            displayBox.Items.Add(currMemory.ToString());
+
+            displayBox.Items.Add("*********************************");
+            displayBox.Items.Add("Total Price: " + totalPrice.ToString());
+        }
+    }
+}
