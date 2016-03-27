5db590ee91f3513b579e6470e370c8fb352a2447
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-27 18:35:17 +1300

    Added necessary variables, constructor and getters/setters

--- WeatherStation/WeatherStation/WeatherSubject.cs
+++ WeatherStation/WeatherStation/WeatherSubject.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace WeatherStation
 {
-    class WeatherSubject
+    public class WeatherSubject : ISubject
     {
+        //Variables
+        private List<IObserver> weatherList;
+        private int currentTemp;
+        private int currentPressure;
+        private int currentHumidity;
+
+        //Constructor
+        public WeatherSubject()
+        {
+            // Inititalise all the variables
+            currentTemp = 0;
+            currentHumidity = 0;
+            currentPressure = 0;
+            weatherList = new List<IObserver>();
+        }
+
+        //Getters/Setters
+        public int CurrentTemp
+        {
+            get { return currentTemp; }
+            set { currentTemp = value; }
+        }
+
+        public int CurrentPressure
+        {
+            get { return currentPressure; }
+            set { currentPressure = value; }
+        }
+
+        public int CurrentHumidity
+        {
+            get { return currentHumidity; }
+            set { currentHumidity = value; }
+        }
     }
 }
