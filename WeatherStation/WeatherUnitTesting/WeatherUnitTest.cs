6699d530482fbe3154804549ee8340d363ee151f
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-28 16:11:55 +1300

    Unit test the average reading for temperature

---
+++ WeatherStation/WeatherUnitTesting/WeatherUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherStation;
+using System.Windows.Forms;
+
+
+namespace WeatherUnitTesting
+{
+    [TestClass]
+    public class WeatherUnitTest
+    {
+        [TestMethod]
+        public void Update_AvgReadings_ReturnExpectedValue()
+        {
+            ListBox lb = new ListBox();
+            WeatherSubject subject = new WeatherSubject();
+            AverageObserver aO = new AverageObserver(lb, subject);
+
+            aO.Update(12, 1, 1);
+            aO.Update(20, 1, 1);
+
+            double expected = 16;
+            double actual = aO.ComputedTemp;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
