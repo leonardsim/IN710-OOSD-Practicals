56557555b46c5751abf9527ab1351f1b784b6ca5
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-28 23:29:36 +1300

    Updated SocialObserver with base code

--- SocialNetworking/SocialNetworking/SocialObserver.cs
+++ SocialNetworking/SocialNetworking/SocialObserver.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SocialNetworking
 {
-    class SocialObserver
+    public abstract class SocialObserver : IObserver
     {
+        //Attributes
+        protected string currentStatus;
+        protected DateTime currentTimeStamp;
+        protected ListBox lb;
+        protected SocialSubject ss;
+
+        //Constructor
+        public SocialObserver(ListBox lb, SocialSubject ss)
+        {
+            // Inititalise attributes
+            currentStatus = "";
+            currentTimeStamp = DateTime.Now;
+
+            this.lb = lb;
+            this.ss = ss;
+
+            ss.AddObserver(this);
+        }
+
+        //Getter/Setter
+        public string CurrentStatus
+        {
+            get { return currentStatus; }
+            set { currentStatus = value; }
+        }
+
+        public DateTime CurrentTimeStamp
+        {
+            get { return currentTimeStamp; }
+            set { currentTimeStamp = value; }
+        }
     }
 }
