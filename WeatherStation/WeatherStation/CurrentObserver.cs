1540837aaca4d3b0cb1e41a937be392767e114e4
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-28 13:35:50 +1300

    Created the CurrentObserver sub-class

---
+++ WeatherStation/WeatherStation/CurrentObserver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherStation
+{
+    public class CurrentObserver : WeatherObserver
+    {
+        //Constructor
+        public CurrentObserver(ListBox lb, WeatherSubject subject)
+            : base (lb, subject)
+        {
+
+        }
+    }
+}
