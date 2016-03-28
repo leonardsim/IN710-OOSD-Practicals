8c3f3c3443ee76b7fe6560dc9f4e1d30668867b1
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-28 13:41:23 +1300

    Created variables needed to calculate the average

---
+++ WeatherStation/WeatherStation/AverageObserver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    public class AverageObserver : WeatherObserver
+    {
+        //Variables
+        int runningTemp;
+        int runningHumidity;
+        int runningPressure;
+        int avgCount;
+
+        //Constructor
+    }
+}
