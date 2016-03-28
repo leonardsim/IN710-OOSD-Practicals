9930917d8bb3f4605e28c56192ddaba445638880
leonardsim <simle1@STUDENT.op.ac.nz>
2016-03-28 22:59:53 +1300

    Added methods to the subject interface

--- SocialNetworking/SocialNetworking/ISubject.cs
+++ SocialNetworking/SocialNetworking/ISubject.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace SocialNetworking
 {
-    interface ISubject
+    public interface ISubject
     {
+        void AddObserver(IObserver o);
+        void RemoveObserver(IObserver o);
+        void NotifyObservers();
     }
 }
