ccecb73b468175ceb4086b625d4ad3111bbfed03
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-03 23:07:52 +1200

    Creadted DelegateScore method that will display the score depending on which team was picked

--- QuizNight/QuizNight/Form1.cs
+++ QuizNight/QuizNight/Form1.cs
@@ -12,9 +12,44 @@ namespace QuizNight
 {
     public partial class Form1 : Form
     {
+        // Declare delegate type 
+        public delegate int ScoreDelegate(int correct, int incorrect);
+        ScoreDelegate scoreComputer;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Instantiate the delegate and pass the Scorer methods. 
+        // Set the score to the score label
+        private void DelegateScore()
+        {
+            scoreComputer = null;
+
+            if (rdoAdult.Checked)
+            {
+                scoreComputer = new ScoreDelegate(Scorer.AdultScore);
+            }
+            else if (rdoChild.Checked)
+            {
+                scoreComputer = new ScoreDelegate(Scorer.ChildScore);
+            }
+            else
+            {
+                MessageBox.Show("Please pick a team!");
+            }
+
+            // Parse text to int
+            int correctAns = Int32.Parse(txtCorrect.Text.ToString());
+            int incorrectAns = Int32.Parse(txtIncorrect.Text.ToString());
+
+            // Pass correct and incorrect answer as arguement to the delegate and 
+            // save it as result value
+            int result = scoreComputer(correctAns, incorrectAns);
+
+            // Set the result to the score label
+            lblScore.Text = "Score: " + result;
         }
     }
 }
