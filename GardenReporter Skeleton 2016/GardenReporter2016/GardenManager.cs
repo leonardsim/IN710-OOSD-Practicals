e899fd1f454b854f9e83d5b282f6d1a55e6b510c
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-04 11:18:55 +1200

    Added base code for GardenManager class

---
+++ GardenReporter Skeleton 2016/GardenReporter2016/GardenManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenReporter2016
+{
+    public class GardenManager
+    {
+        //Attributes
+        List<Garden> gardenList;
+
+        //Constructor
+        public GardenManager()
+        {
+            gardenList = new List<Garden>();
+        }
+    }
+}
