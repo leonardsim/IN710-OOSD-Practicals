05a4c4757b8d411368ddafbf07bb308560576638
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-03 17:37:09 +1200

    Created slowMethod to slow the update progress and updated the btnStart lick to run the method

--- ProgressFeedbackDisplay/ProgressFeedbackDisplay/Form1.cs
+++ ProgressFeedbackDisplay/ProgressFeedbackDisplay/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,8 +13,9 @@ namespace ProgressFeedbackDisplay
 {
     public partial class Form1 : Form
     {
-        //Declare delegate
+        // Declare delegate
         public delegate void FeedbackDelegate();
+
 
         public Form1()
         {
@@ -37,6 +39,43 @@ namespace ProgressFeedbackDisplay
         public void updateTrackBar()
         {
             trackBar1.Value++;
+        }
+
+        // Will slow down the progress
+        public void slowMethod(FeedbackDelegate feedback)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                feedback();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Reset all the feedback values
+            numericUpDown1.Value = 0;
+            progressBar1.Value = 0;
+            trackBar1.Value = 0;
+
+            // Create FeedbackDelegate instance
+            FeedbackDelegate feedback = null;
+
+            // Bind delegate dynamically
+            if (rdoSpin.Checked)
+            {
+                feedback = new FeedbackDelegate(updateSpinBox);
+            }
+            else if (rdoProg.Checked)
+            {
+                feedback = new FeedbackDelegate(updateProgressBar);
+            }
+            else if (rdoTrack.Checked)
+            {
+                feedback = new FeedbackDelegate(updateTrackBar);
+            }
+
+            slowMethod(feedback);
         }
     }
 }
