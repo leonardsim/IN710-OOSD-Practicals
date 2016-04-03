568b5846577f9528747aff7e3bfbd103f9175e8d
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-04 11:47:27 +1200

    Added ProcessGardens method (Incomplete)
    Need to find a way to pass gardenDelegate delegate into the
    ProcessGardens argument

--- GardenReporter Skeleton 2016/GardenReporter2016/GardenManager.cs
+++ GardenReporter Skeleton 2016/GardenReporter2016/GardenManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GardenReporter2016
 {
@@ -22,6 +23,19 @@ namespace GardenReporter2016
         public void AddGarden(Garden garden)
         {
             gardenList.Add(garden);
+        }
+        
+        // Adds the garden information to the lsit depending on the method delegate
+        public void ProcessGardens(Delegate gardenDelegate, ListBox lb)
+        {
+            // Clear the listbox
+            lb.Items.Clear();
+
+            // Itterrate through the list
+            foreach(Garden g in gardenList)
+            {
+                lb.Items.Add();
+            }
         }
     }
 }
