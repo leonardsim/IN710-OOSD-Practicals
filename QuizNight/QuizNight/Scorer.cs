d84a3d5288c6e0db8dc7b99d85be6521aeb8ef28
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-03 22:53:17 +1200

    Created AdultScore method to calculate adult score

--- QuizNight/QuizNight/Scorer.cs
+++ QuizNight/QuizNight/Scorer.cs
@@ -8,5 +8,18 @@ namespace QuizNight
 {
     public static class Scorer
     {
+        //Methods
+        // Returns the adult score
+        public static int AdultScore(int correct, int incorrect)
+        {
+            // declare and inititalise score
+            int score = 0;
+
+            // For every correct answer multiply score by 10 and for incorrect, multiply by 1 
+            // then deduct both of them
+            score = ((correct * 10) - (incorrect * 1));
+
+            return score;
+        }
     }
 }
