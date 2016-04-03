175026c7cf1ac56be24eec3eee4501144b866887
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-03 22:36:46 +1200

    Updated form design

--- QuizNight/QuizNight/Form1.Designer.cs
+++ QuizNight/QuizNight/Form1.Designer.cs
@@ -28,12 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoAdult = new System.Windows.Forms.RadioButton();
+            this.rdoChild = new System.Windows.Forms.RadioButton();
+            this.lblAdult = new System.Windows.Forms.Label();
+            this.lblChild = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdoCorrect = new System.Windows.Forms.RadioButton();
+            this.rdoWrong = new System.Windows.Forms.RadioButton();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoChild);
+            this.groupBox1.Controls.Add(this.rdoAdult);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(80, 77);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Teams";
+            // 
+            // rdoAdult
+            // 
+            this.rdoAdult.AutoSize = true;
+            this.rdoAdult.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoAdult.Location = new System.Drawing.Point(7, 20);
+            this.rdoAdult.Name = "rdoAdult";
+            this.rdoAdult.Size = new System.Drawing.Size(54, 17);
+            this.rdoAdult.TabIndex = 0;
+            this.rdoAdult.TabStop = true;
+            this.rdoAdult.Text = "Adults";
+            this.rdoAdult.UseVisualStyleBackColor = true;
+            // 
+            // rdoChild
+            // 
+            this.rdoChild.AutoSize = true;
+            this.rdoChild.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoChild.Location = new System.Drawing.Point(7, 44);
+            this.rdoChild.Name = "rdoChild";
+            this.rdoChild.Size = new System.Drawing.Size(63, 17);
+            this.rdoChild.TabIndex = 1;
+            this.rdoChild.TabStop = true;
+            this.rdoChild.Text = "Children";
+            this.rdoChild.UseVisualStyleBackColor = true;
+            // 
+            // lblAdult
+            // 
+            this.lblAdult.AutoSize = true;
+            this.lblAdult.Location = new System.Drawing.Point(128, 33);
+            this.lblAdult.Name = "lblAdult";
+            this.lblAdult.Size = new System.Drawing.Size(39, 13);
+            this.lblAdult.TabIndex = 1;
+            this.lblAdult.Text = "Adults:";
+            // 
+            // lblChild
+            // 
+            this.lblChild.AutoSize = true;
+            this.lblChild.Location = new System.Drawing.Point(119, 59);
+            this.lblChild.Name = "lblChild";
+            this.lblChild.Size = new System.Drawing.Size(48, 13);
+            this.lblChild.TabIndex = 2;
+            this.lblChild.Text = "Children:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(127, 13);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(40, 13);
+            this.lblScore.TabIndex = 3;
+            this.lblScore.Text = "Score";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "What is 2 + 2?";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdoWrong);
+            this.groupBox2.Controls.Add(this.rdoCorrect);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(13, 96);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(184, 88);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Question";
+            // 
+            // rdoCorrect
+            // 
+            this.rdoCorrect.AutoSize = true;
+            this.rdoCorrect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoCorrect.Location = new System.Drawing.Point(7, 36);
+            this.rdoCorrect.Name = "rdoCorrect";
+            this.rdoCorrect.Size = new System.Drawing.Size(31, 17);
+            this.rdoCorrect.TabIndex = 6;
+            this.rdoCorrect.TabStop = true;
+            this.rdoCorrect.Text = "4";
+            this.rdoCorrect.UseVisualStyleBackColor = true;
+            // 
+            // rdoWrong
+            // 
+            this.rdoWrong.AutoSize = true;
+            this.rdoWrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdoWrong.Location = new System.Drawing.Point(7, 60);
+            this.rdoWrong.Name = "rdoWrong";
+            this.rdoWrong.Size = new System.Drawing.Size(31, 17);
+            this.rdoWrong.TabIndex = 7;
+            this.rdoWrong.TabStop = true;
+            this.rdoWrong.Text = "2";
+            this.rdoWrong.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(216, 105);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(142, 79);
+            this.btnPlay.TabIndex = 7;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 205);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblChild);
+            this.Controls.Add(this.lblAdult);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoChild;
+        private System.Windows.Forms.RadioButton rdoAdult;
+        private System.Windows.Forms.Label lblAdult;
+        private System.Windows.Forms.Label lblChild;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdoWrong;
+        private System.Windows.Forms.RadioButton rdoCorrect;
+        private System.Windows.Forms.Button btnPlay;
     }
 }
 
