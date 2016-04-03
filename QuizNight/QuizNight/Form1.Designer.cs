e66175f2f56fb86da5bec50a7c58ef173538472b
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-03 22:51:07 +1200

    Re-updated design

--- QuizNight/QuizNight/Form1.Designer.cs
+++ QuizNight/QuizNight/Form1.Designer.cs
@@ -34,13 +34,10 @@
             this.lblAdult = new System.Windows.Forms.Label();
             this.lblChild = new System.Windows.Forms.Label();
             this.lblScore = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.rdoCorrect = new System.Windows.Forms.RadioButton();
-            this.rdoWrong = new System.Windows.Forms.RadioButton();
             this.btnPlay = new System.Windows.Forms.Button();
+            this.txtCorrect = new System.Windows.Forms.TextBox();
+            this.txtIncorrect = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -82,94 +79,62 @@
             // lblAdult
             // 
             this.lblAdult.AutoSize = true;
-            this.lblAdult.Location = new System.Drawing.Point(128, 33);
+            this.lblAdult.Location = new System.Drawing.Point(120, 28);
             this.lblAdult.Name = "lblAdult";
-            this.lblAdult.Size = new System.Drawing.Size(39, 13);
+            this.lblAdult.Size = new System.Drawing.Size(87, 13);
             this.lblAdult.TabIndex = 1;
-            this.lblAdult.Text = "Adults:";
+            this.lblAdult.Text = "Correct Answers:";
             // 
             // lblChild
             // 
             this.lblChild.AutoSize = true;
-            this.lblChild.Location = new System.Drawing.Point(119, 59);
+            this.lblChild.Location = new System.Drawing.Point(112, 61);
             this.lblChild.Name = "lblChild";
-            this.lblChild.Size = new System.Drawing.Size(48, 13);
+            this.lblChild.Size = new System.Drawing.Size(95, 13);
             this.lblChild.TabIndex = 2;
-            this.lblChild.Text = "Children:";
+            this.lblChild.Text = "Incorrect Answers:";
             // 
             // lblScore
             // 
             this.lblScore.AutoSize = true;
-            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblScore.Location = new System.Drawing.Point(127, 13);
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(176, 112);
             this.lblScore.Name = "lblScore";
-            this.lblScore.Size = new System.Drawing.Size(40, 13);
+            this.lblScore.Size = new System.Drawing.Size(38, 13);
             this.lblScore.TabIndex = 3;
-            this.lblScore.Text = "Score";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 19);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(76, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "What is 2 + 2?";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.rdoWrong);
-            this.groupBox2.Controls.Add(this.rdoCorrect);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(13, 96);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(184, 88);
-            this.groupBox2.TabIndex = 6;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Question";
-            // 
-            // rdoCorrect
-            // 
-            this.rdoCorrect.AutoSize = true;
-            this.rdoCorrect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdoCorrect.Location = new System.Drawing.Point(7, 36);
-            this.rdoCorrect.Name = "rdoCorrect";
-            this.rdoCorrect.Size = new System.Drawing.Size(31, 17);
-            this.rdoCorrect.TabIndex = 6;
-            this.rdoCorrect.TabStop = true;
-            this.rdoCorrect.Text = "4";
-            this.rdoCorrect.UseVisualStyleBackColor = true;
-            // 
-            // rdoWrong
-            // 
-            this.rdoWrong.AutoSize = true;
-            this.rdoWrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rdoWrong.Location = new System.Drawing.Point(7, 60);
-            this.rdoWrong.Name = "rdoWrong";
-            this.rdoWrong.Size = new System.Drawing.Size(31, 17);
-            this.rdoWrong.TabIndex = 7;
-            this.rdoWrong.TabStop = true;
-            this.rdoWrong.Text = "2";
-            this.rdoWrong.UseVisualStyleBackColor = true;
+            this.lblScore.Text = "Score:";
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(216, 105);
+            this.btnPlay.Location = new System.Drawing.Point(13, 106);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(142, 79);
+            this.btnPlay.Size = new System.Drawing.Size(81, 24);
             this.btnPlay.TabIndex = 7;
             this.btnPlay.Text = "Play";
             this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // txtCorrect
+            // 
+            this.txtCorrect.Location = new System.Drawing.Point(213, 25);
+            this.txtCorrect.Name = "txtCorrect";
+            this.txtCorrect.Size = new System.Drawing.Size(100, 20);
+            this.txtCorrect.TabIndex = 8;
+            // 
+            // txtIncorrect
+            // 
+            this.txtIncorrect.Location = new System.Drawing.Point(213, 56);
+            this.txtIncorrect.Name = "txtIncorrect";
+            this.txtIncorrect.Size = new System.Drawing.Size(100, 20);
+            this.txtIncorrect.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(370, 205);
+            this.ClientSize = new System.Drawing.Size(331, 139);
+            this.Controls.Add(this.txtIncorrect);
+            this.Controls.Add(this.txtCorrect);
             this.Controls.Add(this.btnPlay);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.lblChild);
             this.Controls.Add(this.lblAdult);
@@ -178,8 +143,6 @@
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -193,11 +156,9 @@
         private System.Windows.Forms.Label lblAdult;
         private System.Windows.Forms.Label lblChild;
         private System.Windows.Forms.Label lblScore;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RadioButton rdoWrong;
-        private System.Windows.Forms.RadioButton rdoCorrect;
         private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtCorrect;
+        private System.Windows.Forms.TextBox txtIncorrect;
     }
 }
 
