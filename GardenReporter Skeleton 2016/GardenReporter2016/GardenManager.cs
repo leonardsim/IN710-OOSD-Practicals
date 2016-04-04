cb5bd73e7b43aa9612ad5e1182303ad802b916b5
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-04 12:39:54 +1200

    Formatted the string output to be added to the listbox

--- GardenReporter Skeleton 2016/GardenReporter2016/GardenManager.cs
+++ GardenReporter Skeleton 2016/GardenReporter2016/GardenManager.cs
@@ -11,11 +11,15 @@ namespace GardenReporter2016
     {
         //Attributes
         List<Garden> gardenList;
+        public delegate double gardenDelegate();
+        gardenDelegate gD;
+        String reportString;
 
         //Constructor
         public GardenManager()
         {
             gardenList = new List<Garden>();
+            reportString = "";
         }
 
         //Method
@@ -26,7 +30,7 @@ namespace GardenReporter2016
         }
         
         // Adds the garden information to the lsit depending on the method delegate
-        public void ProcessGardens(Delegate gardenDelegate, ListBox lb)
+        public void ProcessGardens(gardenDelegate gD, ListBox lb, Button bt)
         {
             // Clear the listbox
             lb.Items.Clear();
@@ -34,8 +38,28 @@ namespace GardenReporter2016
             // Itterrate through the list
             foreach(Garden g in gardenList)
             {
-                lb.Items.Add();
+                // Depending on the button name, the garden method will be passed into the delegate
+                if(bt.Name == "btnArea")
+                {
+                    gD = new gardenDelegate(g.GetArea);
+                    reportString = String.Format("{0,-14}:{1,8:f2}", g.OwnerName, g.GetArea());
+                }
+                else if (bt.Name == "btnCharges")
+                {
+                    gD = new gardenDelegate(g.GetAccountBalance);
+                    reportString = String.Format("{0,-14}:{1,8:f2}", g.OwnerName, g.GetAccountBalance());
+                }
+
+                // Add to the list
+                lb.Items.Add(reportString);
             }
+        }
+
+        // Getter/Setter
+        public gardenDelegate GD
+        {
+            get { return gD; }
+            set { gD = value; }
         }
     }
 }
