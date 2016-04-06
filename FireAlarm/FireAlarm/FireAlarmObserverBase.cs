cf209c06f05fc7d13aaa82fbb99f329b04c981a2
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-06 23:14:00 +1200

    Created FireAlarmObserverBase (Incomplete constructor)

---
+++ FireAlarm/FireAlarm/FireAlarmObserverBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    public abstract class FireAlarmObserverBase
+    {
+        // Declare subject
+        protected FireAlarmSubject sub;
+
+        //Constructor
+        public FireAlarmObserverBase(FireAlarmSubject sub)
+        {
+            this.sub = sub;
+
+            FireAlarmSubject.FireEventHandler handler = new FireAlarmSubject.FireEventHandler();
+        }
+    }
+}
