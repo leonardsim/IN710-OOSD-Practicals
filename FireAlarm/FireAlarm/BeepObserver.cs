9ea18d5a086e98a82dbb3dca2d12343e9ee67fa7
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 00:54:37 +1200

    Created BeepObserver sub class with base code

---
+++ FireAlarm/FireAlarm/BeepObserver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    public class BeepObserver : FireAlarmObserverBase
+    {
+        //Import beep method from kernel32 library
+        [DllImport("kernel32.dll")]
+
+        // Beep method
+        public static extern bool Beep(int freq, int duration);
+
+        //Constructor
+        public BeepObserver(FireAlarmSubject sub) : base(sub)
+        {
+
+        }
+
+        //Method
+        public override void FireAlarmHandlerMethod(object fireSubject, FireEventArgs fa)
+        {
+            // Beep duration and frequency
+            int duration = 1000;
+            int frequency = 0;
+
+            switch(fa.FireCategory)
+            {
+                case EFireCategory.MINOR:
+                    frequency = 800;
+                    break;
+                case EFireCategory.SERIOUS:
+                    frequency = 1200;
+                    break;
+                case EFireCategory.INFERNO:
+                    frequency = 1600;
+                    break;
+            }
+
+            Beep(frequency, duration);
+        }
+    }
+}
