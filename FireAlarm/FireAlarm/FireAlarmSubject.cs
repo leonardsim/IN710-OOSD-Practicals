a9cbf20beb90ad5a51c98e64ff22918d5e090bf7
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-06 20:31:02 +1200

    Created FireAlarmSubject with base code

---
+++ FireAlarm/FireAlarm/FireAlarmSubject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    public class FireAlarmSubject
+    {
+        // Declare delegate
+        public delegate void FireEventHandler(object fireSubject, FireEventArgs fe);
+        
+        // Declare event
+        public event FireEventHandler FireEvent;
+
+        
+    }
+}
