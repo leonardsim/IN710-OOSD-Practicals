663ae9702d5a649d7566e7eb249573d9c44775b1
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 00:05:35 +1200

    Created InstructionObserver sub class with basecode

---
+++ FireAlarm/FireAlarm/InstructionObserver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FireAlarm
+{
+    public class InstructionObserver : FireAlarmObserverBase
+    {
+        //Constructor
+        public InstructionObserver(FireAlarmSubject sub) : base(sub)
+        {
+
+        }
+
+        //Method
+        public override void FireAlarmHandlerMethod(object fireSubject, FireEventArgs fa)
+        {
+            // Feedback message
+            String message = "Fire category is " + fa.FireCategory.ToString() + ".";
+
+            // Depending on which enum was picked, a str will be concat'd to the message
+            switch(fa.FireCategory)
+            {
+                case EFireCategory.MINOR:
+                    message += " Use a fire extinguisher!";
+                    break;
+                case EFireCategory.SERIOUS:
+                    message += " Call the fire department!";
+                    break;
+                case EFireCategory.INFERNO:
+                    message += " Evacuate immediately!!";
+                    break;
+            }
+
+            // Message box will appear
+            MessageBox.Show(message);
+        }
+    }
+}
