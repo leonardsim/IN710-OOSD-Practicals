01b2afd0735880dc55901c87359a14012ee8dc84
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-06 20:27:04 +1200

    Created FireEventArgs sub class with base code

---
+++ FireAlarm/FireAlarm/FireEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireAlarm
+{
+    // Declare enum
+    public enum EFireCategory { MINOR, SERIOUS, INFERNO };
+
+    public class FireEventArgs : EventArgs
+    {
+        // Create enum accessor
+        public EFireCategory FireCategory { get; set; }
+
+        //Constructor
+        public FireEventArgs(EFireCategory currentFireCategory)
+        {
+            FireCategory = currentFireCategory;
+        }
+    }
+}
