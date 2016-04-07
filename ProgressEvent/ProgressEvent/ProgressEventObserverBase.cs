c1aefbfb5163f06f423be7c7015a7a2857ea3a01
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 13:35:01 +1200

    Created ProgressEventObserverBase class with base code

---
+++ ProgressEvent/ProgressEvent/ProgressEventObserverBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressEvent
+{
+    public abstract class ProgressEventObserverBase
+    {
+        // Declare subject
+        protected ProgressEventSubject subject;
+
+        //Constructor
+        public ProgressEventObserverBase(ProgressEventSubject subject)
+        {
+            this.subject = subject;
+
+            EventHandler handler = new EventHandler(UpdateProgress);
+
+            subject.ProgressEvent += handler;
+        }
+
+        //Method
+        public abstract void UpdateProgress(object sender, EventArgs e);
+    }
+}
