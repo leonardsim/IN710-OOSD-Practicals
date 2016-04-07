83f9a460659d9b3d05cc5fc1e21eb7a49dbfc968
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 12:14:28 +1200

    Made a string builder to hold all the display message

--- CustomControlHandler/CustomControlHandler/CustomControlObserver.cs
+++ CustomControlHandler/CustomControlHandler/CustomControlObserver.cs
@@ -23,7 +23,14 @@ namespace CustomControlHandler
         //Method to pass into delegate event handler
         public void CustomControlHandlerMethod(object sender, EventArgs e)
         {
-            
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("This is a custom handler");
+            sb.AppendLine("My code number is " + codeNum);
+            sb.AppendLine("My type is " + this.GetType().ToString());
+            sb.AppendLine("I am responding to button: " + button.Name);
+
+            // Display to MessageBox
+            MessageBox.Show(sb.ToString());
         }
 
     }
