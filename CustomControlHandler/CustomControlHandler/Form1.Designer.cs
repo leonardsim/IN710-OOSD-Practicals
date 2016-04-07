d5c2b8a0dab7701e48b483977a005a1b43669a44
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 12:12:41 +1200

    Added name design to button

--- CustomControlHandler/CustomControlHandler/Form1.Designer.cs
+++ CustomControlHandler/CustomControlHandler/Form1.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnTest
             // 
-            this.button1.Location = new System.Drawing.Point(13, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 84);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Test Handler";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnTest.Location = new System.Drawing.Point(13, 13);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(194, 84);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test Handler";
+            this.btnTest.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(219, 109);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnTest);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -54,7 +54,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnTest;
     }
 }
 
