b8c96cc725fcdb18517e56509f525c5d7248e76e
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 13:56:15 +1200

    Created TrackBarObserver sub class with base code

---
+++ ProgressEvent/ProgressEvent/TrackBarObserver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressEvent
+{
+    public class TrackBarObserver : ProgressEventObserverBase
+    {
+        //Attributes
+        TrackBar track;
+
+        //Constrcutor
+        public TrackBarObserver(ProgressEventSubject subject, TrackBar track) : base(subject)
+        {
+            this.track = track;
+        }
+
+        //Method
+        public override void UpdateProgress(object sender, EventArgs e)
+        {
+            track.Value++;
+        }
+    }
+}
