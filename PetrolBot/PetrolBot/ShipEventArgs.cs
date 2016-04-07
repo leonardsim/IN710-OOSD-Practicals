85e047959ae342678fb40cc3ded361cbeb71748c
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 14:38:15 +1200

    Created ShipEventArgs class with base code

---
+++ PetrolBot/PetrolBot/ShipEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetrolBot
+{
+    public class ShipEventArgs : EventArgs
+    {
+        //Attribute
+        public Point ShipLocation {get; set;}
+
+        //Constructor
+        public ShipEventArgs(Point ShipLocation)
+        {
+            this.ShipLocation = ShipLocation;
+        }
+    }
+}
