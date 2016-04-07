a97a17dbd4e652dfec0b5a5c0a144f20e29c63ce
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 19:03:21 +1200

    Got the ship and background drawn to the form

--- PetrolBot/PetrolBot/Form1.cs
+++ PetrolBot/PetrolBot/Form1.cs
@@ -12,9 +12,48 @@ namespace PetrolBot
 {
     public partial class Form1 : Form
     {
+        //Attributes
+        Graphics mainCanvas;
+        Brush backgroundBrush;
+        Brush petrolBackground;
+        List<Ship> shipList;
+        List<PetrolBot> petrolList;
+        Random rand;
+ 
+
         public Form1()
         {
             InitializeComponent();
+
+            //Inititalise them
+            mainCanvas = CreateGraphics();
+            rand = new Random();
+            backgroundBrush = new SolidBrush(Color.Gray);
+            petrolBackground = new SolidBrush(Color.Black);
+            shipList = new List<Ship>();
+            petrolList = new List<PetrolBot>();
+
+            //Inititalise Ships
+            Ship s1 = new Ship(mainCanvas, rand);
+            Ship s2 = new Ship(mainCanvas, rand);
+
+            //Add to Ship List
+            shipList.Add(s1);
+            shipList.Add(s2);
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Draw the background
+            mainCanvas.FillRectangle(backgroundBrush, 0, 0, 600, 600);
+            mainCanvas.FillRectangle(petrolBackground, 0, 500, 600, 200);
+
+            // Draws the ship
+            foreach (Ship s in shipList)
+            {
+                s.drawShip();
+            }
         }
     }
 }
