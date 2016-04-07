1a939c9e4f31cc07b7e89e51c0184ab5320579a8
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 12:09:43 +1200

    Created CustomControlObserver class with base code

---
+++ CustomControlHandler/CustomControlHandler/CustomControlObserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomControlHandler
+{
+    public class CustomControlObserver
+    {
+        //Attributes
+        private Button button;
+        private int codeNum;
+
+        //Constructor
+        public CustomControlObserver(Button button, int codeNum)
+        {
+            this.button = button;
+            this.codeNum = codeNum;
+        }
+
+        //Method to pass into delegate event handler
+        public void CustomControlHandlerMethod(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
