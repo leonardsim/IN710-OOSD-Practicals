a4033f92be9accd9a2a97175173bfd527fec636b
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 13:39:49 +1200

    Created SpinBoxObserver sub class with base code

---
+++ ProgressEvent/ProgressEvent/SpinBoxObserver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressEvent
+{
+    public class SpinBoxObserver : ProgressEventObserverBase
+    {
+        //Attributes
+        NumericUpDown spinBox;
+
+        //Constructor
+        public SpinBoxObserver(ProgressEventSubject subject, NumericUpDown spinBox) : base(subject)
+        {
+            this.spinBox = spinBox;
+        }
+
+        //Method
+        public override void UpdateProgress(object sender, EventArgs e)
+        {
+            spinBox.Value++;
+        }
+    }
+}
