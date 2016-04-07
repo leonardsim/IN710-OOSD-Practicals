1c217dcf2aabf2d1bb2c08372ce17ac301960647
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 15:00:19 +1200

    Created PetrolBot class with base code (Incomplete)

---
+++ PetrolBot/PetrolBot/PetrolBot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetrolBot
+{
+    public class PetrolBot
+    {
+        //Attributes
+        Graphics botCanvas;
+        Color botColor;
+        Point botStartingLocation;
+        Point botCurrentLocation;
+        Ship botShip;
+        Point shipLocation;
+
+        //Constructor
+        public PetrolBot(Graphics botCanvas, Color botColor, Ship botShip)
+        {
+            this.botCanvas = botCanvas;
+            this.botColor = botColor;
+            this.botShip = botShip;
+        }
+
+        //Methods
+        public void drawBot()
+        {
+
+        }
+
+        public void FullOfFuelEventhandler()
+        {
+
+        }
+
+        public void OutOfFuelEventHandler()
+        {
+
+        }
+
+    }
+}
