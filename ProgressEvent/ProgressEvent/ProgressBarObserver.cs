bf867a8e4193330f5ec14a1e0ba165e3d80da6ca
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-07 13:52:56 +1200

    Created ProgressBarObserver sub class with base code

---
+++ ProgressEvent/ProgressEvent/ProgressBarObserver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressEvent
+{
+    public class ProgressBarObserver : ProgressEventObserverBase
+    {
+        //Attribute
+        ProgressBar progress;
+
+        //Constructor
+        public ProgressBarObserver(ProgressEventSubject subject, ProgressBar progress) : base(subject)
+        {
+            this.progress = progress;
+        }
+
+        //Method
+        public override void UpdateProgress(object sender, EventArgs e)
+        {
+            progress.PerformStep();
+        }
+    }
+}
