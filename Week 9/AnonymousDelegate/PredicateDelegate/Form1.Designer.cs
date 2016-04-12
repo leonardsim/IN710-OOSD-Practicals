82e723e69f19f8870290519c4c0cdf9f660064b8
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-12 22:45:36 +1200

    Copied PredicateDelegate project to be redone

---
+++ Week 9/AnonymousDelegate/PredicateDelegate/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PredicateDelegate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRand = new System.Windows.Forms.Button();
+            this.btnOdd = new System.Windows.Forms.Button();
+            this.btnTen = new System.Windows.Forms.Button();
+            this.lbRand = new System.Windows.Forms.ListBox();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnRand
+            // 
+            this.btnRand.Location = new System.Drawing.Point(13, 13);
+            this.btnRand.Name = "btnRand";
+            this.btnRand.Size = new System.Drawing.Size(146, 35);
+            this.btnRand.TabIndex = 0;
+            this.btnRand.Text = "Generate Random ";
+            this.btnRand.UseVisualStyleBackColor = true;
+            this.btnRand.Click += new System.EventHandler(this.btnRand_Click);
+            // 
+            // btnOdd
+            // 
+            this.btnOdd.Location = new System.Drawing.Point(257, 13);
+            this.btnOdd.Name = "btnOdd";
+            this.btnOdd.Size = new System.Drawing.Size(146, 35);
+            this.btnOdd.TabIndex = 1;
+            this.btnOdd.Text = "Select Odd Numbers";
+            this.btnOdd.UseVisualStyleBackColor = true;
+            this.btnOdd.Click += new System.EventHandler(this.btnEven_Click);
+            // 
+            // btnTen
+            // 
+            this.btnTen.Location = new System.Drawing.Point(257, 54);
+            this.btnTen.Name = "btnTen";
+            this.btnTen.Size = new System.Drawing.Size(146, 35);
+            this.btnTen.TabIndex = 2;
+            this.btnTen.Text = "Select Numbers < 10";
+            this.btnTen.UseVisualStyleBackColor = true;
+            this.btnTen.Click += new System.EventHandler(this.btnTen_Click);
+            // 
+            // lbRand
+            // 
+            this.lbRand.FormattingEnabled = true;
+            this.lbRand.Location = new System.Drawing.Point(13, 106);
+            this.lbRand.Name = "lbRand";
+            this.lbRand.Size = new System.Drawing.Size(186, 212);
+            this.lbRand.TabIndex = 3;
+            // 
+            // lbResult
+            // 
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.Location = new System.Drawing.Point(217, 106);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(186, 212);
+            this.lbResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 336);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.lbRand);
+            this.Controls.Add(this.btnTen);
+            this.Controls.Add(this.btnOdd);
+            this.Controls.Add(this.btnRand);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRand;
+        private System.Windows.Forms.Button btnOdd;
+        private System.Windows.Forms.Button btnTen;
+        private System.Windows.Forms.ListBox lbRand;
+        private System.Windows.Forms.ListBox lbResult;
+    }
+}
+
---
+++ Week 9/AnonymousDelegate/PredicateDelegate/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PredicateDelegate
+{
+    public partial class Form1 : Form
+    {
+        //Attributes
+        List<int> numList;
+        Random rand;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Instantiate attributes
+            numList = new List<int>();
+            rand = new Random();
+        }
+
+        //Method
+        // Checks to see if the input is an odd number
+        private bool isOdd(int inputInteger)
+        {
+            bool isAnOddNumber = ((inputInteger % 2) == 1);
+            return isAnOddNumber;
+        }
+
+        // Checks to see if the input is less than 10
+        private bool isLessThan(int inputInteger)
+        {
+            bool isLessThanTen;
+
+            if (inputInteger < 10)
+            {
+                isLessThanTen = true;
+            }
+            else
+            {
+                isLessThanTen = false;
+            }
+
+            return isLessThanTen;
+        }
+
+        private void generateRand()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                // Generate number between 0 - 100
+                int randNum = rand.Next(101);
+
+                // Add the number into the list
+                numList.Add(randNum);
+            }
+        }
+
+        private void btnRand_Click(object sender, EventArgs e)
+        {
+            // Clear number list and list boxes
+            numList.Clear();
+            lbResult.Items.Clear();
+            lbRand.Items.Clear();
+
+            generateRand();
+
+            foreach (int i in numList)
+            {
+                lbRand.Items.Add(i);
+            }
+        }
+
+        private void btnEven_Click(object sender, EventArgs e)
+        {
+            // Clear list box
+            lbResult.Items.Clear();
+
+            // Declare, initialize and instantiate Predicate
+            Predicate<int> isOddPredicate = new Predicate<int>(isOdd);
+
+            // Call FindAll method which accepts a Predicate and returns a list of int composed
+            // of all elelemts of the target list for which the Predicate returns true
+            List<int> results = numList.FindAll(isOddPredicate);
+
+            // Display it onto the listbox
+            foreach (int r in results)
+            {
+                lbResult.Items.Add(r);
+            }
+            
+        }
+
+        private void btnTen_Click(object sender, EventArgs e)
+        {
+            // Clear list box
+            lbResult.Items.Clear();
+
+            // Declare, initialize and instantiate Predicate
+            Predicate<int> isLessThanPredicate = new Predicate<int>(isLessThan);
+
+            // Call FindAll method which accepts a Predicate and returns a list of int composed
+            // of all elelemts of the target list for which the Predicate returns true
+            List<int> results = numList.FindAll(isLessThanPredicate);
+
+            // Display it onto the listbox
+            foreach (int r in results)
+            {
+                lbResult.Items.Add(r);
+            }
+        }
+    }
+}
