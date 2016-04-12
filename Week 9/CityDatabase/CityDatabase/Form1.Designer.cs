2e6a4bad56baae0dde703d4680d08a704aa7f7c1
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-12 23:38:15 +1200

    Created CityDatabase project

---
+++ Week 9/CityDatabase/CityDatabase/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CityDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCities = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbCities
+            // 
+            this.lbCities.FormattingEnabled = true;
+            this.lbCities.Location = new System.Drawing.Point(201, 13);
+            this.lbCities.Name = "lbCities";
+            this.lbCities.Size = new System.Drawing.Size(228, 264);
+            this.lbCities.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 290);
+            this.Controls.Add(this.lbCities);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbCities;
+    }
+}
+
