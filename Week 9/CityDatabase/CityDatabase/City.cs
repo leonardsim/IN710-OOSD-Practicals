cfedd1109695bdf217dba98b63d5ad8fa0010e5b
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-12 23:38:49 +1200

    Created City class with base code

---
+++ Week 9/CityDatabase/CityDatabase/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityDatabase
+{
+    public class City
+    {
+        //Accessors
+        public string CityName { get; set; }
+        public string CountryName { get; set; }
+        public int Population { get; set; }
+
+        //Constructor
+        public City(string cityName, string countryName, int population)
+        {
+            CityName = cityName;
+            CountryName = countryName;
+            Population = population;
+        }
+    }
+}
