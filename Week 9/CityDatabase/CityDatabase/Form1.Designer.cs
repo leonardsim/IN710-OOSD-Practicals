fe170df75779bdf28145f18e3227e4a7f72fe16e
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-13 12:36:36 +1200

    Created populateLists method

--- Week 9/CityDatabase/CityDatabase/Form1.Designer.cs
+++ Week 9/CityDatabase/CityDatabase/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.lbCities = new System.Windows.Forms.ListBox();
             this.cbCountry = new System.Windows.Forms.ComboBox();
             this.btnDisplay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbCities
@@ -44,7 +45,7 @@
             // cbCountry
             // 
             this.cbCountry.FormattingEnabled = true;
-            this.cbCountry.Location = new System.Drawing.Point(13, 13);
+            this.cbCountry.Location = new System.Drawing.Point(13, 29);
             this.cbCountry.Name = "cbCountry";
             this.cbCountry.Size = new System.Drawing.Size(182, 21);
             this.cbCountry.TabIndex = 2;
@@ -58,17 +59,28 @@
             this.btnDisplay.Text = "Display Cities";
             this.btnDisplay.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Countries:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(441, 290);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnDisplay);
             this.Controls.Add(this.cbCountry);
             this.Controls.Add(this.lbCities);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +89,7 @@
         private System.Windows.Forms.ListBox lbCities;
         private System.Windows.Forms.ComboBox cbCountry;
         private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Week 9/CityDatabase/CityDatabase/Form1.cs
+++ Week 9/CityDatabase/CityDatabase/Form1.cs
@@ -12,9 +12,50 @@ namespace CityDatabase
 {
     public partial class Form1 : Form
     {
+        // Declare city & country list
+        List<City> cityList;
+        List<String> countryList;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Instantiate them
+            cityList = new List<City>();
+            countryList = new List<String>();
+
+            // Call populateLists()
+            populateLists();
+        }
+
+        //Method
+        public void populateLists()
+        {
+            // Instantiate cities
+            // Malaysian cities
+            City cKK = new City("Kota Kinabalu", "Malaysia", 2000000);
+            City cKuc = new City("Kuching", "Malaysia", 2000000);
+
+            // UK cities
+            City cLon = new City("London", "United Kingdom", 2000000);
+            City cMan = new City("Machester", "United Kingdom", 2000000);
+
+            // NZ cities
+            City cDun = new City("Dunedin", "New Zealand", 2000000);
+            City CAuc = new City("Auckland", "New Zealand", 2000000);
+
+            // Add them to the city list
+            cityList.Add(cKK);
+            cityList.Add(cKuc);
+            cityList.Add(cLon);
+            cityList.Add(cMan);
+            cityList.Add(cDun);
+            cityList.Add(CAuc);
+
+            // Add the city's country propertie to the country list
+            countryList.Add(cKK.CountryName);
+            countryList.Add(cLon.CountryName);
+            countryList.Add(CAuc.CountryName);
         }
     }
 }
