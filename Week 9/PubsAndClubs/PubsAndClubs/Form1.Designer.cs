5dc963a95adb851283f975a823de920ed282fca7
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-17 23:59:35 +1200

    Added new DataGridView and button

--- Week 9/PubsAndClubs/PubsAndClubs/Form1.Designer.cs
+++ Week 9/PubsAndClubs/PubsAndClubs/Form1.Designer.cs
@@ -37,7 +37,14 @@
             this.btnInfo = new System.Windows.Forms.Button();
             this.btnRock = new System.Windows.Forms.Button();
             this.btnMonth = new System.Windows.Forms.Button();
+            this.dgvBand = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.role = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.instruments = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvGigs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBand)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvGigs
@@ -51,7 +58,7 @@
             this.time});
             this.dgvGigs.Location = new System.Drawing.Point(13, 13);
             this.dgvGigs.Name = "dgvGigs";
-            this.dgvGigs.Size = new System.Drawing.Size(504, 150);
+            this.dgvGigs.Size = new System.Drawing.Size(504, 119);
             this.dgvGigs.TabIndex = 0;
             // 
             // bandName
@@ -81,7 +88,7 @@
             // 
             // btnInfo
             // 
-            this.btnInfo.Location = new System.Drawing.Point(13, 169);
+            this.btnInfo.Location = new System.Drawing.Point(13, 266);
             this.btnInfo.Name = "btnInfo";
             this.btnInfo.Size = new System.Drawing.Size(114, 44);
             this.btnInfo.TabIndex = 1;
@@ -91,7 +98,7 @@
             // 
             // btnRock
             // 
-            this.btnRock.Location = new System.Drawing.Point(133, 169);
+            this.btnRock.Location = new System.Drawing.Point(133, 266);
             this.btnRock.Name = "btnRock";
             this.btnRock.Size = new System.Drawing.Size(114, 44);
             this.btnRock.TabIndex = 2;
@@ -101,7 +108,7 @@
             // 
             // btnMonth
             // 
-            this.btnMonth.Location = new System.Drawing.Point(253, 169);
+            this.btnMonth.Location = new System.Drawing.Point(253, 266);
             this.btnMonth.Name = "btnMonth";
             this.btnMonth.Size = new System.Drawing.Size(114, 44);
             this.btnMonth.TabIndex = 3;
@@ -109,11 +116,60 @@
             this.btnMonth.UseVisualStyleBackColor = true;
             this.btnMonth.Click += new System.EventHandler(this.btnMonth_Click);
             // 
+            // dgvBand
+            // 
+            this.dgvBand.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBand.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.firstName,
+            this.lastName,
+            this.role,
+            this.instruments});
+            this.dgvBand.Location = new System.Drawing.Point(12, 138);
+            this.dgvBand.Name = "dgvBand";
+            this.dgvBand.Size = new System.Drawing.Size(505, 119);
+            this.dgvBand.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 44);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Alabama Shakes members";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // firstName
+            // 
+            this.firstName.HeaderText = "First Name";
+            this.firstName.Name = "firstName";
+            this.firstName.Width = 82;
+            // 
+            // lastName
+            // 
+            this.lastName.HeaderText = "Last Name";
+            this.lastName.Name = "lastName";
+            this.lastName.Width = 83;
+            // 
+            // role
+            // 
+            this.role.HeaderText = "Role";
+            this.role.Name = "role";
+            this.role.Width = 54;
+            // 
+            // instruments
+            // 
+            this.instruments.HeaderText = "Instruments";
+            this.instruments.Name = "instruments";
+            this.instruments.Width = 86;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 225);
+            this.ClientSize = new System.Drawing.Size(529, 322);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvBand);
             this.Controls.Add(this.btnMonth);
             this.Controls.Add(this.btnRock);
             this.Controls.Add(this.btnInfo);
@@ -121,6 +177,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dgvGigs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBand)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -136,6 +193,12 @@
         private System.Windows.Forms.Button btnInfo;
         private System.Windows.Forms.Button btnRock;
         private System.Windows.Forms.Button btnMonth;
+        private System.Windows.Forms.DataGridView dgvBand;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn firstName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn role;
+        private System.Windows.Forms.DataGridViewTextBoxColumn instruments;
     }
 }
 
