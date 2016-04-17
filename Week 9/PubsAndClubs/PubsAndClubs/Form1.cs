77dbb5ec4ab4692155a5bcfd36b75c14d53f77f2
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-17 22:51:47 +1200

    Added displayHardRock method

--- Week 9/PubsAndClubs/PubsAndClubs/Form1.cs
+++ Week 9/PubsAndClubs/PubsAndClubs/Form1.cs
@@ -52,9 +52,42 @@ namespace PubsAndClubs
             }
         }
 
+        private void displayHardRock()
+        {
+            //CLear data grid view rows
+            dgvRows.Clear();
+
+            // Get the root element
+            XElement evGuide = bandDoc.Element("Event_Guide");
+
+            foreach(XElement gig in evGuide.Elements("Gig"))
+            {
+                if(gig.Element("Band").Element("Genre").Value.Trim().Equals("Hard Rock"))
+                {
+                    string bandName = gig.Element("Band").Element("Name").Value;
+                    string genre = gig.Element("Band").Element("Genre").Value;
+                    string venue = gig.Element("Venue").Value;
+                    string date = gig.Element("Date").Attribute("day").Value + "/"
+                        + gig.Element("Date").Attribute("month").Value + "/"
+                        + gig.Element("Date").Attribute("year").Value;
+                    string time = gig.Element("Time").Value;
+
+                    String[] newRowValue = { bandName, genre, venue, date, time };
+
+                    // Add the string array to the row
+                    dgvRows.Add(newRowValue);
+                }
+            }
+        }
+
         private void btnInfo_Click(object sender, EventArgs e)
         {
             displayBandInfo();
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            displayHardRock();
         }
     }
 }
