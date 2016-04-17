6af9f7c15ed5ab04b51b0a6a9372e47cbfdcb1fa
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-17 23:18:09 +1200

    Added displayCurrentMonthGig method

--- Week 9/PubsAndClubs/PubsAndClubs/Form1.cs
+++ Week 9/PubsAndClubs/PubsAndClubs/Form1.cs
@@ -80,6 +80,40 @@ namespace PubsAndClubs
             }
         }
 
+        private void displayCurrentMonthGig()
+        {
+            //CLear data grid view rows
+            dgvRows.Clear();
+
+            // Declare and inititalise date
+            DateTime d = DateTime.Now;
+
+            // Save current month in a string
+            string currentMonth = d.ToString("MM");
+
+            // Get the root element
+            XElement evGuide = bandDoc.Element("Event_Guide");
+
+            foreach(XElement gig in evGuide.Elements("Gig"))
+            {
+                if(gig.Element("Date").Attribute("month").Value.Trim().Equals(currentMonth))
+                {
+                    string bandName = gig.Element("Band").Element("Name").Value;
+                    string genre = gig.Element("Band").Element("Genre").Value;
+                    string venue = gig.Element("Venue").Value;
+                    string date = gig.Element("Date").Attribute("day").Value + "/"
+                        + gig.Element("Date").Attribute("month").Value + "/"
+                        + gig.Element("Date").Attribute("year").Value;
+                    string time = gig.Element("Time").Value;
+
+                    String[] newRowValue = { bandName, genre, venue, date, time };
+
+                    // Add the string array to the row
+                    dgvRows.Add(newRowValue);
+                }
+            }
+        }
+
         private void btnInfo_Click(object sender, EventArgs e)
         {
             displayBandInfo();
@@ -88,6 +122,11 @@ namespace PubsAndClubs
         private void btnRock_Click(object sender, EventArgs e)
         {
             displayHardRock();
+        }
+
+        private void btnMonth_Click(object sender, EventArgs e)
+        {
+            displayCurrentMonthGig();
         }
     }
 }
