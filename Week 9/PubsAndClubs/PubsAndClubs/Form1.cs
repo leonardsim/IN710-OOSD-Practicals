ce72bcd7a686ed2ca92be4b3479cc89652ccd0a2
leonardsim <simle1@STUDENT.op.ac.nz>
2016-04-17 22:44:11 +1200

    Added displayBandInfo method

--- Week 9/PubsAndClubs/PubsAndClubs/Form1.cs
+++ Week 9/PubsAndClubs/PubsAndClubs/Form1.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace PubsAndClubs
 {
     public partial class Form1 : Form
     {
+        // Declare DataGridViewRowCollection & XDocument
+        DataGridViewRowCollection dgvRows;
+        XDocument bandDoc;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Inititalise them
+            bandDoc = XDocument.Load("pubsAndClubs.xml");
+            dgvRows = dgvGigs.Rows;
+        }
+
+        //Methods
+        private void displayBandInfo()
+        {
+            //CLear data grid view rows
+            dgvRows.Clear();
+
+            // Get the root element
+            XElement evGuide = bandDoc.Element("Event_Guide");
+
+            foreach(XElement gig in evGuide.Elements("Gig"))
+            {
+                string bandName = gig.Element("Band").Element("Name").Value;
+                string genre = gig.Element("Band").Element("Genre").Value;
+                string venue = gig.Element("Venue").Value;
+                string date = gig.Element("Date").Attribute("day").Value + "/"
+                    + gig.Element("Date").Attribute("month").Value + "/"
+                    + gig.Element("Date").Attribute("year").Value;
+                string time = gig.Element("Time").Value;
+
+                String[] newRowValue = { bandName, genre, venue, date, time };
+
+                // Add the string array to the row
+                dgvRows.Add(newRowValue);
+            }
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            displayBandInfo();
         }
     }
 }
