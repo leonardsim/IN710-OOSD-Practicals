07c55ca176cf1b6ffb356b2ac2608c639d0b526e
leonardsim <simle1@STUDENT.op.ac.nz>
2016-05-11 23:14:02 +1200

    Created connectToDatabase method

--- Week 11/ADOdatabase/ADOdatabase/DatabaseManager.cs
+++ Week 11/ADOdatabase/ADOdatabase/DatabaseManager.cs
@@ -11,5 +11,27 @@ namespace ADOdatabase
     {
         // Declare SqlConnection
         SqlConnection bitdevConnection;
+        
+        //Constructor
+        public DatabaseManager()
+        {
+
+        }
+
+        // Methods
+        // Connects to the database
+        private void connectToDatabase()
+        {
+            bitdevConnection = new SqlConnection();
+
+            bitdevConnection.ConnectionString = "Data Source = bitdev.ict.op.ac.nz;" +
+                                                "Initital Catalog = IN700001_201601_SIMLE1;" +
+                                                "User ID = simle1;" +
+                                                "Password = " + UserInfo.PASS + ";";
+
+            bitdevConnection.Open();
+        }
+
+
     }
 }
