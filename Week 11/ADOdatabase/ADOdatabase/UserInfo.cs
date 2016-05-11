bc58a5a6f21b3fbb8f7de2f0fc92035a6775d306
leonardsim <simle1@STUDENT.op.ac.nz>
2016-05-11 22:39:23 +1200

    Created static class to hold constants

---
+++ Week 11/ADOdatabase/ADOdatabase/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOdatabase
+{
+    public static class UserInfo
+    {
+        public const string PASS = "LSi_2D22";
+    }
+}
