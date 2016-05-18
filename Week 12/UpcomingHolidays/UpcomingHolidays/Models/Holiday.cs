f43b4fb149afbe82291198b7444b1a90100d284c
leonardsim <simle1@STUDENT.op.ac.nz>
2016-05-18 22:51:57 +1200

    Created Holiday Model Class with base code

--- Week 12/UpcomingHolidays/UpcomingHolidays/Models/Holiday.cs
+++ Week 12/UpcomingHolidays/UpcomingHolidays/Models/Holiday.cs
@@ -7,5 +7,20 @@ namespace UpcomingHolidays.Models
 {
     public class Holiday
     {
+        // Properties
+        public string Name { get; set; }
+        public DateTime HolidayDate { get; set; }
+        public int NumDaysToHoliday { get; set; }
+        public string ImageName { get; set; }
+
+        // Constructor
+        public Holiday (string name, DateTime holidayDate, string imageName)
+        {
+            Name = name;
+            HolidayDate = holidayDate;
+            ImageName = imageName;
+
+            NumDaysToHoliday = 0;
+        }
     }
 }
