e998e4cc6f19b39844d7fbc0783328f1dd35ebe6
leonardsim <simle1@STUDENT.op.ac.nz>
2016-05-23 13:59:16 +1200

    Created enum class with base enum classes

---
+++ Week 12/DogSelector/DogSelector/Models/Enum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DogSelector.Models
+{
+    // Enum classes
+    public enum EScale { Low, Medium, High, NoPref }
+    public enum ELength { Short, Medium, Long }
+    public enum ESize { Miniature, Small, Medium, Large, Giant }
+}
